bb24c10df55930773a38a634f2261d4e096868bb
swaiba-bilal <swaibabilal02@gmail.com>
2025-08-05 16:38:47 +0500

    Added solution for LeetCode 1 - Two Sum

---
+++ Easy/1_TwoSum.java
@@ -0,0 +1,22 @@
+/**
+ * LeetCode Problem 1: Two Sum
+ * Link: https://leetcode.com/problems/two-sum/
+ *
+ * Given an array of integers nums and an integer target, 
+ * return indices of the two numbers such that they add up to target.
+ *
+ * Approach: Brute force with nested loops to check all pairs.
+ */
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
